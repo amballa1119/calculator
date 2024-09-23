f42c186827dced3de86854ff4bfce957e146db85
amballa jesubabu <156279832+amballa1119@users.noreply.github.com>
2024-09-23 19:27:50 +0530

    Add files via upload

---
+++ realcal.js
@@ -0,0 +1,18 @@
+console.log(eval("2*4+6-10/2"));
+console.log(Number("20"));
+console.log(Number("20+30"));
+
+let m="kishore"
+console.log(m.length);
+
+let k=m.slice(0,m.length-1)
+console.log(k);// it returns the "kishor" excludes last digit e.
+
+let a=[3,4,5,6,7,8]
+console.log(a);
+
+delete a[0]
+console.log(a);
+console.log(a[0]);
+
+
